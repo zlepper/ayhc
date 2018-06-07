8b704a37a9e55c55094f5d1ddceb732078080b7a
Zlepper <hansen13579@gmail.com>
2018-06-07 20:46:24 +0200

    Attempt to fix parses not somewhat fuzzy matching on content type, and thus being able to ignore encoding
    take 2

--- response-parsers.go
+++ response-parsers.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"strings"
 )
 
 // A ResponseParser should convert a response body into an object
@@ -25,9 +26,19 @@ func RegisterResponseParser(contentType string, parser ResponseParser) {
 // Parses the response body into the v object
 // v should be a pointer
 func ParseResponse(contentType string, body io.Reader, v interface{}) error {
+	contentType = strings.ToLower(contentType)
+
+	// Try to get a registered parser
+	// First attempt a full match
 	parser, exists := responseParsers[contentType]
 	if !exists {
-		return errors.New(fmt.Sprintf("no request parser found for content type '%s'", contentType))
+		// next attempt a match on the content type only
+		rawType := strings.Split(contentType, ";")[0]
+		parser, exists = responseParsers[rawType]
+		if !exists {
+			// If that didn't work either, then fail full out
+			return errors.New(fmt.Sprintf("no response parser found for content type '%s'", contentType))
+		}
 	}
 
 	return parser.Convert(body, v)
