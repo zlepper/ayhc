68a271cba060b045dde27326d75e98fb477618da
Zlepper <hansen13579@gmail.com>
2018-06-07 20:44:10 +0200

    Attempt to fix parses not somewhat fuzzy matching on content type, and thus being able to ignore encoding

--- request-parsers.go
+++ request-parsers.go
@@ -3,6 +3,7 @@ package yahc
 import (
 	"errors"
 	"fmt"
+	"strings"
 )
 
 // A request parser will take to the request body struct, and turn it into
@@ -17,17 +18,26 @@ var requestParsers = make(map[string]RequestParser)
 // Registers a new parser for the given contentType
 // If a parser is already registered, it's overwritten
 func RegisterRequestParser(contentType string, parser RequestParser) {
-	requestParsers[contentType] = parser
+	requestParsers[strings.ToLower(contentType)] = parser
 }
 
 // Convert the body into bytes
 // Will attempt to convert into the requested content type
 // If no matching parser is registered, will return an error
 func ConvertRequestBody(contentType string, body interface{}) (data []byte, err error) {
+	contentType = strings.ToLower(contentType)
+
 	// Try to get a registered parser
+	// First attempt a full match
 	parser, exists := requestParsers[contentType]
 	if !exists {
-		return nil, errors.New(fmt.Sprintf("no request parser found for content type '%s'", contentType))
+		// next attempt a match on the content type only
+		rawType := strings.Split(contentType, ";")[0]
+		parser, exists = requestParsers[rawType]
+		if !exists {
+			// If that didn't work either, then fail full out
+			return nil, errors.New(fmt.Sprintf("no request parser found for content type '%s'", contentType))
+		}
 	}
 
 	// Actually convert the data
